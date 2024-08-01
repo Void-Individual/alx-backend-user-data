04bd7c8832ba96422c8a907eed05f604f0cd3bb5
Void-Individual <otayomitv@gmail.com>
2024-08-01 22:47:38 +0100

    Type annotated the function

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,10 +2,12 @@
 """Function to return obfuscated log message"""
 
 import re
+from typing import List
 
 
 # pattern = r'\b(' + '|'.join(fields) + r'[;]'
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
     pattern = r'|'.join(f'{field}=[^{separator}]+' for field in fields)
     return re.sub(pattern, lambda m: f"{m.group().split('=')[0]}={redaction}",
                   message)
