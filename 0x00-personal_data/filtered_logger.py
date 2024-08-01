cbfce821337d3b4c95c29e854ae240359c61706c
Void-Individual <otayomitv@gmail.com>
2024-08-01 23:07:45 +0100

    Implemented format method

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,6 +3,26 @@
 
 import re
 from typing import List
+import logging
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        message = record.getMessage()
+        record.msg = filter_datum(self.fields, self.REDACTION, message,
+                                  self.SEPARATOR)
+        return super().format(record)
 
 
 # pattern = r'\b(' + '|'.join(fields) + r'[;]'
