cff5df952c588e74cdc58461ba77c7f4d323863c
Void-Individual <otayomitv@gmail.com>
2024-08-03 21:09:55 +0100

    Modded documentation

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,7 +7,7 @@ import logging
 
 
 class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class
+    """ Redacting Formatter class to create a log of user data
         """
 
     REDACTION = "***"
