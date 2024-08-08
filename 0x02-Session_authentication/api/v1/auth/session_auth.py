792732e0b4f77cb5a09b835b88e172516e52be5e
Void-Individual <otayomitv@gmail.com>
2024-08-08 17:57:55 +0100

    changed session id from start

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -19,14 +19,14 @@ class SessionAuth(Auth):
         if user_id is None or not isinstance(user_id, str):
             return None
 
-        session_id = uuid.uuid4()
-        self.user_id_by_session_id[str(session_id)] = user_id
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
         return session_id
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
         """Method that returns a User ID based on a Session ID"""
 
-        session_id = str(session_id)
+        # session_id = str(session_id)
         if session_id is None or not isinstance(session_id, str):
             return None
         user_id = self.user_id_by_session_id.get(session_id, None)
