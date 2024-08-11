6e2fd6fd24ff2a5bd84a7329c3ff8532a555ba9a
Void-Individual <otayomitv@gmail.com>
2024-08-11 06:59:33 +0100

    If no user_id, return None

--- 0x02-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -41,6 +41,7 @@ class SessionDBAuth(SessionExpAuth):
             for session in sessions:
                 if session.user_id is None:
                     session.remove()
+                    return None
                 return session.user_id
         return None
 
