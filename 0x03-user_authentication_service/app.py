497462b58bb7b487394d5263cbe94060a8986e78
Void-Individual <otayomitv@gmail.com>
2024-08-19 14:52:24 +0100

    Complete switch

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -100,21 +100,26 @@ def get_reset_password_token():
         abort(403)
 
 
-@app.route('/reset_password', methods=['PUT'], strict_slashes=False)
-def update_password():
-    """Method to collect the passed email, reset token and new password
-    to update the user db. If the token is invalid, catch the exception
-    and raise a 403else, return with json payload and 200"""
+@app.route("/reset_password", methods=["PUT"], strict_slashes=False)
+def update_password() -> str:
+    """PUT /reset_password
 
-    email = request.form.get('email')
-    reset_token = request.form.get('reset_token')
-    new_password = request.form.get('new_password')
-
+    Return:
+        - The user's password updated payload.
+    """
+    email = request.form.get("email")
+    reset_token = request.form.get("reset_token")
+    new_password = request.form.get("new_password")
+    is_password_changed = False
     try:
         AUTH.update_password(reset_token, new_password)
-        return jsonify({"email": email, "message": "Password updated"})
+        is_password_changed = True
     except ValueError:
+        is_password_changed = False
+    if not is_password_changed:
         abort(403)
+    return jsonify({"email": email, "message": "Password updated"})
+
 
 
 if __name__ == "__main__":
